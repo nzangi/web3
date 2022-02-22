70460bb7337090ca5f71243895f91ec26fb20110
nzangi <ext install yurihs.sublime-vscode-theme>
2022-02-22 13:06:42 +0300

    Social media aacounts added at footer page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,7 @@
+import React from "react";
 import logo from '../../images/logo.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faFacebook,faTwitter,faInstagram,faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return(
@@ -25,11 +28,31 @@ const Footer = () => {
                 <p className = "text-white text-sm text-center font-bold">nzangimuoki@gmail.com <br/> annanzaga@gmail.com </p>
 
             </div>
-            <div className = "sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5"/>
+
+            <div className="text-white flex-col flex-row  cursor-pointer justify-between items-center mt-3">
+                <a href = "https://www.twitter.com/NzangiMuoki" className="p-3 space-between color-[#49a1eb]">
+                    <FontAwesomeIcon icon = {faTwitter} size = "2x" color = "#49a1eb"/>
+                </a>
+                <a href="https://www.instagram.com/muokinzangi"
+                    className="p-3 space-between">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" color = "#fb3958"/>
+                </a>
+                <a href="https://www.facebook.com/nzangi.muoky/"
+                    className="p-3 space-between">
+                    <FontAwesomeIcon icon={faFacebook} size="2x"color = '#4968ad' />
+                </a>
+                <a href="https://www.youtube.com/"
+                    className="space-between p-3">
+                    <FontAwesomeIcon icon={faYoutube} size="2x" color = "#eb3223"/>
+                </a>
+            </div>
+
+
+            <div className = "sm:w-[90%] w-full  h-[0.25px] bg-gray-200 mt-5"/>
             <div className = "sm:w-[90%] w-full flex justify-between items-center mt-3">
                 <p className = "text-white text-sm text-center font-bold">@DigitalCoin</p>
                 <p className = "text-white text-sm text-center font-bold italic text-decoration underline mt-1">Developed By Nzangi Muoki 19/02/2022</p>
-                <p className = "text-white  text-sm text-center font-bold">All Rights Reserved</p>
+                <p className = "text-white text-sm text-center font-bold">All Rights Reserved</p>
 
 
             </div>
